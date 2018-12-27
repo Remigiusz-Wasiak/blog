e7689fb260861ce06d0abc02977b4021ef5a4178
rw <remigiusz.wasiak@gmail.com>
2018-12-27 22:29:12 +0100

    Header component created

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,57 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Header from './Header/Header'
 
 import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
+    const { location, title, children } = this.props;
     return (
       <div
         style={{
@@ -61,7 +16,7 @@ class Layout extends React.Component {
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        {header}
+        <Header />
         {children}
         <footer>
           © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
