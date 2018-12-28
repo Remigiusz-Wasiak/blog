1531745a54021800aaf1b4e01e7959121cc06841
rw <remigiusz.wasiak@gmail.com>
2018-12-28 16:33:03 +0100

    styling update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,49 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styles from './Header.module.scss'
-const ListLink = props => (
-  <li className={ styles.menu__item }>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+import styled from 'styled-components'
+
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 36rem;
+  padding-bottom: 1.25rem;
+`
+
+const Logo = styled.h3`
+  display: inline;
+  text-shadow: none;
+  background-image: none;
+`
+
+const List = styled.ul`
+  display: flex;
+  margin: 0;
+  list-style: none;
+`
+
+const StyledLink = styled.li`
+  margin-bottom: 0;
+  margin-left: 1.25rem;
+`
 
 export default () => (
-  <div className={ styles.wrapper }>
-    <header className={ styles.header }>
-      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>Remigiusz Wasiak</h3>
+    <Header>
+      <Link to="/">
+        <Logo>RW</Logo>
       </Link>
-      <ul className={ styles.menu }>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-  </div>
+      <List>
+          <StyledLink>
+              <Link to="/">Home</Link>
+          </StyledLink>
+          <StyledLink>
+              <Link to="/about/">About</Link>
+          </StyledLink>
+          <StyledLink>
+              <Link to="/contact/">Contact</Link>
+          </StyledLink>
+      </List>
+    </Header>
 )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Header from './Header/Header'
-
 import { rhythm, scale } from '../utils/typography'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: ${rhythm(24)};
+  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+`
 
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props;
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <Wrapper>
         <Header />
         {children}
         <footer>
           © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Wrapper>
     )
   }
 }
