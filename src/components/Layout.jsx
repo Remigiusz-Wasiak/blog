1b02445cc91c1bb39c97bc533da0ee0331f52633
rw <remigiusz.wasiak@gmail.com>
2018-12-30 14:39:14 +0100

    Layout semantic changed and auto height calucaltion added for main element

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,19 +8,42 @@ const Wrapper = styled.div`
   margin-left: auto;
   margin-right: auto;
   max-width: ${rhythm(26)};
-  padding: ${rhythm(1.5)} ${rhythm(3 / 4)};
+  padding: 0 ${rhythm(3 / 4)};
+`
+const Main = styled.main(props => ({
+  minHeight: `calc(100vh - ${props.usedHeight}px)`,
+  padding: `${rhythm(1.5)} 0`,
+}))
+
+const Footer = styled.footer`
+  display: flex;
+  justify-content: center;
 `
 
 class Layout extends React.Component {
+  state = {
+    usedHeight: null,
+  }
+
+  componentDidMount() {
+    this.setState({
+      usedHeight: this.header.offsetHeight + this.footer.offsetHeight,
+    })
+  }
+
+  getRef = ref => {
+    this.header = ref
+  }
+
   render() {
     const { location, children } = this.props
+    const { usedHeight } = this.state
+
     return (
       <Wrapper>
-        <Header location={location} />
-        {children}
-        <footer>
-          © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Header location={location} getRef={this.getRef} />
+        <Main usedHeight={usedHeight}>{children}</Main>
+        <Footer ref={el => (this.footer = el)}>© 2018 Remigiusz Wasiak</Footer>
       </Wrapper>
     )
   }
