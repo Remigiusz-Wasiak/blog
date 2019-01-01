2d420806abd3306a703b420c2d7ad94581d01b10
rw <remigiusz.wasiak@gmail.com>
2019-01-01 18:49:55 +0100

    pwa favicon replaced, theme color disabled and name changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,13 +58,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `Remigiusz Wasiak Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/rw-icon.png`,
+        theme_color_in_head: false,
       },
     },
     `gatsby-plugin-offline`,
