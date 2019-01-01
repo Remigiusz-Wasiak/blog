7fbd055d6cfd61d3551f56f3a61d1f7894aefbef
rw <remigiusz.wasiak@gmail.com>
2019-01-01 18:50:29 +0100

    new favicon for a blog added

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import favicon from '../../content/assets/favicon.ico'
+import favicon from '../../content/assets/rw-icon.png'
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
