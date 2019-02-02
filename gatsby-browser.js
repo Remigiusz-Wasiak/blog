449a69082e494de1dc9ca3a97670ed50f0df0a68
rw <remigiusz.wasiak@gmail.com>
2019-02-02 11:38:31 +0100

    page reload info added in console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,5 @@ require("prismjs/themes/prism-tomorrow.css")
 
 exports.onServiceWorkerUpdateReady = () => {
   window.location.reload()
+  console.log('service worker update reade - page reloaded');
 }
