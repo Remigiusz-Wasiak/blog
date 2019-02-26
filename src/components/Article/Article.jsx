3bb9982a97ecfa21a24fed84b01584651eaa1019
rw <remigiusz.wasiak@gmail.com>
2019-02-26 13:52:22 +0100

    tags added to Article component, date nad header styles modified

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,25 +3,41 @@ import {Link} from 'gatsby'
 import { rhythm, scale } from '../../utils/typography'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
+import Tags from '../Tags/Tags'
 
 const Wrapper = styled.article`
-  margin-bottom: ${rhythm(3)};
+  &:not(:last-of-type) {
+    margin-bottom: ${rhythm(2)};
+  }
   position: relative;
 `
 
 const ArticleHeader = styled.header`
   margin-bottom: ${rhythm(0.25)};
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  
+  @media(max-width: 535px) {
+    flex-direction: column;
+  }
 `
+const HeaderLink = styled(Link)`
+  flex: 5;
+`
+
 const Header = styled.h2`
   font-size: ${rhythm(1.1)};
   margin-bottom: ${rhythm(0.5)};
   line-height: ${rhythm(1.6)};
 `
 
-const Date = styled.p`
+const Date = styled(Link)`
   ${scale(-1 / 5)};
   margin-bottom: ${rhythm(0.3)};
-  margin-top: ${rhythm(-0.6)};
+  flex: 1;
+  text-align: end;
+  line-height: ${rhythm(1.6)};
 `
 
 const StyledArticleImg = styled(Img)`
@@ -36,13 +52,14 @@ class Article extends React.Component {
     const slug = articleData.node.fields.slug
     const description = articleData.node.excerpt
     const sizes = articleData.node.frontmatter.heroImg ? articleData.node.frontmatter.heroImg.childImageSharp.fluid : null
+    const tags = articleData.node.frontmatter.tags ? articleData.node.frontmatter.tags : null
 
 
     return (
       <Wrapper>
         <ArticleHeader>
-            <Link to={ slug }><Header>{ title }</Header></Link>
-            <Link to={ slug }><Date>{ date }</Date></Link>
+          <HeaderLink to={ slug }><Header>{ title }</Header></HeaderLink>
+          <Date to={ slug }>{ date }</Date>
         </ArticleHeader>
         { sizes && (
           <Link to={ slug }>
@@ -52,6 +69,7 @@ class Article extends React.Component {
           </Link>
         )}
         <Link to={ slug }><p dangerouslySetInnerHTML={{ __html: description }} /></Link>
+        { tags && <Tags tags={tags}/> }
       </Wrapper>
     )
   }
